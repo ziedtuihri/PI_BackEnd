ceb4a7dc520bc56f860bc61702662795b1d9bf50
Zied Touahri <35895902+ziedtuihri@users.noreply.github.com>
2025-03-13 07:12:22 +0100

    services and controller

---
+++ pi/src/main/java/esprit/example/pi/services/IServices.java
@@ -0,0 +1,12 @@
+package esprit.example.pi.services;
+import org.springframework.data.repository.query.Param;
+// import tn.esprit.donation.entities.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+public interface IServices {
+
+
+}
